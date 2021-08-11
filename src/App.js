d222ccde8afb9c08ba5152d157a03dd1be0f9520
Christi-Lee <christileemc@gmail.com>
2021-08-10 18:01:45 -0700

    set up dependencies in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const client = new ApolloClient({
+  uri:
+});
+
+class App extends Component {
+  render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +27,7 @@ function App() {
       </header>
     </div>
   );
+}
 }
 
 export default App;
